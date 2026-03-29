68bf771d9f4917684276f478e5a466874bf0e67d
OliEfr <ollihaus@t-online.de>
2026-03-29 14:35:31 +0200

    test
    update train script

---
+++ scripts/copy_dataset_to_cluster.sh
@@ -0,0 +1,3 @@
+rsync -av --progress \
+/home/admin_07/project_repos/lerobot_realWorldDRL/lerobot/datasets/ \
+ge58guq2@login.ai.lrz.de:/dss/dssmcmlfs01/pn57pi/pn57pi-dss-0001/oliver_hausdoerfer/lerobot/datasets
